045e59d47377d4a7dfc83db57a16ae1fc533ac5d
John Michael Umali <@johnmichaelumali>
2023-04-07 18:45:05 +0800

    login page

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/fdb06bd453.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.cdnfonts.com/css/tolkien" rel="stylesheet">
+  <link rel="stylesheet" href="final.css">
+  <title>Document</title>
+</head>
+<body>
+<section class="vh-100 shadow-sm" style="background-color: #f6f6f6;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-start h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body p-4 text-center">
+            <div class="header">
+              <img src="./img/download.jpg" alt="" class="img">
+              
+              <p class="log-content-p" ><span class="head">Login your Account</span> <br><span>to access the internet</span></p>
+            </div>   
+            <div class="log-body">
+								<div class="form-group myr-top">
+								<label>Username</label>
+									<input type="email" class="form-control custom" placeholder="Username" name = "email">
+								</div>
+
+								<div class="form-group myr-top">
+								<label>Password</label>
+									<input type="password" class="form-control custom" placeholder="Password" name="pwd">
+								</div>
+                <br>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <span> I agree to the <span class="span"> Data Privacy Policy</span></span>
+                </div>
+								<div class="log-btn text-center" style="padding-bottom: 5px;">
+									<a href="#" class="btn btn-theme1" style="margin-top: 5px;"> 
+                   <i class="fa-solid fa-right-to-bracket"></i>
+                     Login
+                    </a>
+								</div>
+                <div class="devider">
+                <hr class="my-4">
+                <p class="text-center">or</p>
+                <hr class="my-4">
+                </div>
+                <div class="log-btn">
+									<a href="#" class="btn btn-theme2 position-relative" style="margin-top: 5px;"> 
+                   <img class="position-absolute start-0" src="./img/google32.png" alt="">
+                     Continue with LV Google Account
+                    </a>
+								</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</body>
+</html>
